b6ebc4eea7bf3399a93137bddfecfb2aed690e82
Mo-Greene <13blueboy13@daum.net>
2022-06-28 22:13:22 +0900

    feat

--- src/main/java/ct1/ct1.java
+++ src/main/java/ct1/ct1.java
@@ -13,9 +13,13 @@ public class ct1 {
             System.out.println(-1);
         } else if(N % 5 == 0){
             System.out.println(N / 5);
-        } else if(N % 5 == 1 || N % 5 == 3){
+        } else if(N % 5 == 1){
             System.out.println((N / 5) + 1);
-        } else if(N % 5 == 2 || N % 5 == 4){
+        } else if(N % 5 == 2){
+            System.out.println((N / 5) + 2);
+        } else if(N % 5 == 3){
+            System.out.println((N / 5) + 1);
+        } else if(N % 5 == 4){
             System.out.println((N / 5) + 2);
         }
     }
